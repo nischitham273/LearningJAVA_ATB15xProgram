43fbdaa3a0afb0a6a211edd4d5bf02d946d1773c
Nischitha <nischitham1234@gamil.com>
2025-12-20 23:08:13 +0530

    key words and identifiers

--- src/ex2_java_basics/Lab004_keyword_identifiers.java
+++ src/ex2_java_basics/Lab004_keyword_identifiers.java
@@ -5,6 +5,9 @@ public class Lab004_keyword_identifiers {
     public static void main(String[] args){
         System.out.println("key words and identifiers");
         int a = 100;
-        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a/0);
+        System.out.print("Hi");
+        System.out.print("Bye");
     }
 }
---
+++ src/ex3_java_basics/Lab001_practiese.java
@@ -0,0 +1,14 @@
+package ex3_java_basics;
+
+public class Lab001_practiese {
+    public static void main(String[] args) {
+        System.out.println("ptacties");
+        int a = 10;
+        float b = 10.00;
+        int c = 10;
+        if (a == c)
+        {
+            System.out.println("equal");
+        }
+    }
+}
