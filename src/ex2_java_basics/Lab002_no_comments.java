51cbb8f2cde508f5117917f12bba7630a4499cbc
Nischitha <nischitham273@gmail.com>
2025-12-20 14:42:26 +0530

    Comment implementation

--- src/ex2_java_basics/Lab002_no_comments.java
+++ src/ex2_java_basics/Lab002_no_comments.java
@@ -1,8 +1,15 @@
 package ex2_java_basics;
 
+import java.lang.reflect.Method;
+
 public interface Lab002_no_comments {
     public static void main(String[] args){
         System.out.println("thiss");
         //System.out.println("thiss");
+        /*
+         *System.out.println("hello NIschitha")
+         *
+         */
+
     }
 }
