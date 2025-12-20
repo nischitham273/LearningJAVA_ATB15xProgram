2114b0a5fd4eed31e9065e80b3982cc94ee9364e
Nischitha <nischitham273@gmail.com>
2025-12-20 15:43:40 +0530

    key words and identifiers

---
+++ src/ex2_java_basics/Lab004_keyword_identifiers.java
@@ -0,0 +1,11 @@
+package ex2_java_basics;
+
+import java.sql.SQLOutput;
+public class Lab004_keyword_identifiers {
+    public static void main(String[] args){
+        System.out.println("key words and identifiers");
+        int a = 100;
+        System.out.println(a);
+        }
+    }
+}
