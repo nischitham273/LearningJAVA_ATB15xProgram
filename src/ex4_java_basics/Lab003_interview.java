8a0e558f3787e0d1b4e6db4b39add25fbc7e89af
Nischitha <nischitham1234@gamil.com>
2025-12-27 23:17:26 +0530

    adding 2 char

---
+++ src/ex4_java_basics/Lab003_interview.java
@@ -0,0 +1,14 @@
+package ex4_java_basics;
+
+public class Lab003_interview {
+    static void main(String[] args) {
+        char a = 'A';
+        char b = 'B';
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println('A' == 65);
+        System.out.println('a' == 97);
+        System.out.println('A' + 'B');
+
+    }
+}
